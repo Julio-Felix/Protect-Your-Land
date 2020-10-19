d2bcd802e23fc70b52227db7871744bac7aa6639
julio_felix_deway <julio.felix@deway.com.br>
2020-10-19 19:10:54 -0300

    Attack Monsters

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -116,7 +116,8 @@ export default class GameScene extends Phaser.Scene {
         slime.healthbar = new HealthBar(this,object.x,object.y,1)
         slime.body.collideWorldBounds = true;
         slime.already_attack = false
-        slime.movement_ac = 300
+        slime.movement_ac = 1
+        slime.setMaxVelocity(150)
         slime.preUpdate = function(time,delta){        
           this.anims.update(time, delta);
           this.healthbar.setXandY(this.x,this.y - 50, 20, 10)
@@ -148,27 +149,27 @@ export default class GameScene extends Phaser.Scene {
 
 
     // JAVALI
-    this.javali = this.physics.add.sprite(90,850,"javali",0)
-    // this.slime.setScale(0.5, 0.5);
-    this.physics.add.collider(this.javali, this.ground);
-    this.javali.setGravityY(600)
-    this.javali.body.collideWorldBounds = true;
-    this.javali_movement = 300
+    // this.javali = this.physics.add.sprite(90,850,"javali",0)
+    // // this.slime.setScale(0.5, 0.5);
+    // this.physics.add.collider(this.javali, this.ground);
+    // this.javali.setGravityY(600)
+    // this.javali.body.collideWorldBounds = true;
+    // this.javali_movement = 300
 
 
-    // JAVALI Animation
-    this.anims.create({
-      key: 'left_javali',
-      frames:this.anims.generateFrameNames('javali', { start: 7, end: 13}),
-      frameRate: 20,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'right_javali',
-      frames: this.anims.generateFrameNames('javali', { start: 0, end: 6}),
-      frameRate: 20,
-      repeat: -1
-    });
+    // // JAVALI Animation
+    // this.anims.create({
+    //   key: 'left_javali',
+    //   frames:this.anims.generateFrameNames('javali', { start: 7, end: 13}),
+    //   frameRate: 20,
+    //   repeat: -1
+    // });
+    // this.anims.create({
+    //   key: 'right_javali',
+    //   frames: this.anims.generateFrameNames('javali', { start: 0, end: 6}),
+    //   frameRate: 20,
+    //   repeat: -1
+    // });
     
     // Cameras Main
     this.camera = this.cameras.main
@@ -197,6 +198,14 @@ export default class GameScene extends Phaser.Scene {
       }
     }, this);
 
+    this.physics.add.overlap(this.slimes, this.player, this.MonsterAttack, function (monsters,player){
+      if(this.CheckIfPlayerIsAttacking() || this.CheckIfPlayerIsImmune()){
+        return false
+      }else{
+        return true
+      }
+    }, this);
+
   }
 
 
@@ -205,7 +214,7 @@ export default class GameScene extends Phaser.Scene {
 
 
   onKeyInput(event){    
-    if(event.code == "KeyA"){
+    if(event.code == "KeyA" && !this.CheckIfPlayerIsImmune()){
       this.player.body.setVelocity(0)
       if(this.player.anims.getCurrentKey() == "left" || this.player.anims.getCurrentKey() == "turn_left"){
         this.player.anims.play('attack_left', true);
@@ -221,18 +230,49 @@ export default class GameScene extends Phaser.Scene {
     if(!monster.already_attack){
       monster.healthbar.decrease_custom(10);
       monster.already_attack = true
-      monster.body.setVelocityY(-400)
-      if (monster.anims.getCurrentKey() == "right_slime") monster.body.velocity.x = -400;
-      if (monster.anims.getCurrentKey() == "left_slime") monster.body.velocity.x = 400;
+      monster.body.setVelocityY(-200)
+      player.immune = true;
+      // monster.movement_ac = (monster.movement_ac * -1) > 0 ? 1 : -1
+      this.time.delayedCall(1000, function() {
+        monster.movement_ac = monster.movement_ac * -1
+        monster.already_attack = false
+        player.immune = false;
+        // enemy.follow = true;
+      }, this);
+      player.body.x > monster.body.x ? monster.movement_ac = -1 : monster.movement_ac = 1
+      // if (monster.anims.getCurrentKey() == "right_slime")monster.movement_ac = -1;
+      // if (monster.anims.getCurrentKey() == "left_slime") monster.movement_ac = 1;
+    }
+    
+  }
 
-      
+  MonsterAttack(monster, player){
+    if(!this.CheckIfPlayerIsImmune()){
+      monster.attacking = true
+      EventCenter.emit('DecreaseLifeOfPlayer',10);
+      player.immune = true;
+      player.body.x > monster.body.x ? monster.movement_ac = 1 : monster.movement_ac = -1
+      // monster.movement_ac = (monster.movement_ac * -1) > 0 ? 1 : -1
+      this.time.delayedCall(1000, function() {
+        player.immune = false;
+        monster.movement_ac = monster.movement_ac * -1
+        monster.attacking = false;
+        // enemy.follow = true;
+      }, this);
+
+
+
+      player.body.setVelocityY(-200)
+      // if (player.anims.getCurrentKey() == "right") player.body.velocity.x = -50;
+      // if (player.anims.getCurrentKey() == "left") player.body.velocity.x = 50;
+
     }
     
   }
 
   
   update(){
-    EventCenter.emit('DecreaseLifeOfPlayer',0.1);
+    
     // let i = this.events.emit('DecreaseLifeOfPlayer',{countDown:1});
     // console.log(i)
     // this.count-=1
@@ -281,7 +321,7 @@ export default class GameScene extends Phaser.Scene {
       this.SlimeMovement(slime)
     },this)
 
-    this.javali.anims.play('left_javali',true)
+    // this.javali.anims.play('left_javali',true)
     
   }
 
@@ -291,20 +331,30 @@ export default class GameScene extends Phaser.Scene {
     if(slime.active){
       // if(slime.body.blocked.down) slime.body.setVelocityY(200)
 
-      if(slime.body.blocked.left) slime.movement_ac=200
+      if(slime.body.blocked.left) slime.movement_ac= 1
   
-      if(slime.body.blocked.right) slime.movement_ac=-200
+      if(slime.body.blocked.right) slime.movement_ac= -1
       
-      if(slime.movement_ac > 0) slime.anims.play('right_slime',true) 
-      else slime.anims.play('left_slime',true)
+      if(slime.movement_ac > 0) {slime.anims.play('right_slime',true); slime.movement_ac+=10}
+      else {slime.anims.play('left_slime',true);slime.movement_ac-=10;}
 
-      if(slime.body.blocked.down && !slime.already_attack) slime.body.setVelocityX(slime.movement_ac)
+      
+      if(slime.attacking || slime.already_attack) slime.body.setVelocityX(slime.movement_ac)
     }
 
+    if(slime.body.blocked.down && !slime.already_attack) slime.body.setAccelerationX(slime.movement_ac)
   }
 
   CheckIfPlayerIsAttacking(){
     if((this.player.anims.getCurrentKey() == "attack_right" || this.player.anims.getCurrentKey() == "attack_left") ){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  CheckIfPlayerIsImmune(){
+    if(this.player.immune){
       return true
     }else{
       return false
